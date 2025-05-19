6ef8dacbb01871523364f7ba831700e83a2bb6d5
hnuikim <hnuikim@smilegate.com>
2025-05-19 15:20:21 +0900

    chore: 파일 이동

--- gateway/src/jwt/roles.decorator.ts
+++ gateway/src/jwt/roles.decorator.ts
@@ -2,4 +2,4 @@ import { SetMetadata } from '@nestjs/common';
 import { Role } from 'src/auth/auth.domain';
 
 export const ROLES_KEY = 'roles';
-export const Roles = (...roles: Role[]) => SetMetadata(ROLES_KEY, roles);
+export const RolesDecorator = (...roles: Role[]) => SetMetadata(ROLES_KEY, roles);
